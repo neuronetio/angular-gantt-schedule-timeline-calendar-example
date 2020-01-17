fe8abf82dd0a18531fff758b6220b6e2bd74cbb2
neuronet.io <neuronet.io@gmail.com>
2020-01-17 20:49:53 +0100

    initial commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,108 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from "@angular/core";
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  selector: "app-root",
+  template: '<gstc [config]="config" [onState]="onState"></gstc>'
 })
-export class AppComponent {
-  title = 'ng-gstc-test';
+export class AppComponent implements OnInit {
+  title = "ng-gstc-test";
+
+  config: any;
+  gstcState: any;
+
+  ngOnInit() {
+    const iterations = 400;
+
+    // GENERATE SOME ROWS
+
+    const rows = {};
+    for (let i = 0; i < iterations; i++) {
+      const withParent = i > 0 && i % 2 === 0;
+      const id = i.toString();
+      rows[id] = {
+        id,
+        label: "Room " + i,
+        parentId: withParent ? (i - 1).toString() : undefined,
+        expanded: false
+      };
+    }
+
+    const dayLen = 24 * 60 * 60 * 1000;
+
+    // GENERATE SOME ROW -> ITEMS
+
+    const items = {};
+    for (let i = 0; i < iterations; i++) {
+      const id = i.toString();
+      const start = new Date().getTime();
+      items[id] = {
+        id,
+        label: "User id " + i,
+        time: {
+          start: start + i * dayLen,
+          end: start + (i + 2) * dayLen
+        },
+        rowId: id
+      };
+    }
+
+    // LEFT SIDE LIST COLUMNS
+
+    const columns = {
+      percent: 100,
+      resizer: {
+        inRealTime: true
+      },
+      data: {
+        label: {
+          id: "label",
+          data: "label",
+          expander: true,
+          isHtml: true,
+          width: 230,
+          minWidth: 100,
+          header: {
+            content: "Room"
+          }
+        }
+      }
+    };
+
+    this.config = {
+      height: 800,
+      list: {
+        rows,
+        columns
+      },
+      chart: {
+        items
+      }
+    };
+  }
+
+  // GET THE GANTT INTERNAL STATE
+
+  onState(state) {
+    this.gstcState = state;
+
+    // YOU CAN SUBSCRIBE TO CHANGES
+
+    this.gstcState.subscribe("config.list.rows", rows => {
+      console.log("rows changed", rows);
+    });
+
+    this.gstcState.subscribe(
+      "config.chart.items.:id",
+      (bulk, eventInfo) => {
+        if (eventInfo.type === "update" && eventInfo.params.id) {
+          const itemId = eventInfo.params.id;
+          console.log(
+            `item ${itemId} changed`,
+            this.gstcState.get("config.chart.items." + itemId)
+          );
+        }
+      },
+      { bulk: true }
+    );
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,13 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import { BrowserModule } from "@angular/platform-browser";
+import { NgModule } from "@angular/core";
+import { GSTCComponent } from "angular-gantt-schedule-timeline-calendar";
 
-import { AppComponent } from './app.component';
+import { AppComponent } from "./app.component";
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule
-  ],
+  declarations: [AppComponent, GSTCComponent],
+  imports: [BrowserModule],
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {}
